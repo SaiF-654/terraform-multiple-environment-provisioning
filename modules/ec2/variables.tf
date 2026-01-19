31dcbde70ff1d349c6f486ab3b64598e32e0fd3d
Saif Ur Rehman <saifizaki456@gmail.com>
2026-01-19 10:42:19 +0000

    files added

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,35 +1,28 @@
 #Create Multiple instances with their username and count
 variable "instance_count" {
   type  = number
-  default = 3
   description = "Number of EC2 instances"
 }
 variable "ec2_username" {
   type = string
-  default = "ec2-user"
 }
 variable "key_name" {
   type = string
-  default = "Terraform-prod-key"
 }
 
 variable "ec2_sg" {
   type = string
-  default = "allow_ssh_prod"
   description = "Security group name"
 }
 variable "ssh_port" {
   type = number
-  default = 22
 }
 
 variable "ami" {
   type = string
-  default = "ami-0bdd88bd06d16ba03"
 }
 variable "instance_type" {
   type = string
-  default = "t3.micro"
 }
 
 variable "tags" {
